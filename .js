d57b1bb25b530952c32640a71bc207d7abcbd289
AleksandrSmirnov92 <ryan00@mail.ru>
2023-03-09 15:57:43 +0300

    Сделал слайдер

---
+++ .js
@@ -0,0 +1,28 @@
+// const productContainer = document.querySelectorAll(".product_container");
+// const card = document.querySelector(".card");
+// const arrowLeft = document.querySelector(".arrows-left");
+// const arrowRight = document.querySelector(".arrows-rigth");
+// let mProducts = [...productContainer];
+
+// arrowLeft.addEventListener("click", () => {
+//   console.log("arrowLeft");
+
+//   slider("Left");
+// });
+// arrowRight.addEventListener("click", () => {
+//   slider("Right");
+// });
+// let slider = (arrow) => {
+//   mProducts.forEach((item) => {
+//     let cardWidth = Math.round(card.getBoundingClientRect().width);
+
+//     if (arrow === "Right") {
+//       item.scrollLeft += cardWidth + 20;
+//       console.log(item.scrollLeft);
+//     }
+//     if (arrow === "Left") {
+//       item.scrollLeft -= cardWidth + 20;
+//       console.log(item.scrollLeft);
+//     }
+//   });
+// };
